1575b17977c08d6e284753ae8ee202a6942714a9
Brian <briand.nester@gmail.com>
2021-03-08 16:51:58 -0500

    push

--- index.js
+++ index.js
@@ -16,11 +16,20 @@ const corsOptions ={
   credentials:true,            
   optionSuccessStatus:200
 };
+
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.json());
+// app.use(express.json());
 app.use("/", router);
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+router.use("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+   });
 
 const contactEmail = nodemailer.createTransport({
   host: "smtp.gmail.com",
